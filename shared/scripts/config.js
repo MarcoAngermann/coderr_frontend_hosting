b54a20d688ccbffc5a69c9bdaeb6860b43d4200c
Marco Angermann <159908924+MarcoAngermann@users.noreply.github.com>
2025-09-16 18:32:24 +0200

    config.js change

--- shared/scripts/config.js
+++ shared/scripts/config.js
@@ -13,17 +13,17 @@ const API_BASE = "http://157.180.86.208/api/";
 const STATIC_BASE_URL = 'http://157.180.86.208/';
 
 
-const LOGIN_URL = API_BASE + 'login/';
-const REGISTER_URL = API_BASE + 'registration/';
-const PROFILE_URL = API_BASE + 'profile/';
-const BUSINESS_PROFILES_URL = API_BASE + 'profiles/business/';
-const CUSTOMER_PROFILES_URL = API_BASE + 'profiles/customer/';
-const REVIEW_URL = API_BASE + 'reviews/';
-const ORDER_URL = API_BASE + 'orders/';
-const OFFER_URL = API_BASE + 'offers/';
-const OFFER_DETAIL_URL = API_BASE + 'offerdetails/';
-const BASE_INFO_URL = API_BASE + 'base-info/';
-const OFFER_INPROGRESS_COUNT_URL = API_BASE + 'order-count/';
-const OFFER_COMPLETED_COUNT_URL = API_BASE + 'completed-order-count/';
+const LOGIN_URL = 'login/';
+const REGISTER_URL = 'registration/';
+const PROFILE_URL = 'profile/';
+const BUSINESS_PROFILES_URL = 'profiles/business/';
+const CUSTOMER_PROFILES_URL = 'profiles/customer/';
+const REVIEW_URL = 'reviews/';
+const ORDER_URL = 'orders/';
+const OFFER_URL = 'offers/';
+const OFFER_DETAIL_URL = 'offerdetails/';
+const BASE_INFO_URL = 'base-info/';
+const OFFER_INPROGRESS_COUNT_URL = 'order-count/';
+const OFFER_COMPLETED_COUNT_URL = 'completed-order-count/';
 
 const PAGE_SIZE = 6
